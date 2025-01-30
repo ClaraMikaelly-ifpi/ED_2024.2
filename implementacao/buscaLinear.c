1df60c336f71280ad731c139599a5825763ba057
Clara <claramikaellyads.1@gmail.com>
2025-01-30 12:53:03 -0300

    Busca Linear

---
+++ implementacao/buscaLinear.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int buscaÇinear(int arr[], int tamanho, int elemento) {
+    for (int i = 0; i < tamanho; i++) {
+        if (arr[i] == elemento) {
+            return i;         // retorna o índice do elemento encontrado 
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 10, 40};
+    int tamanho = sizeof(arr) / sizeof(arr[0]);
+    int elemento = 10;
+    int resultado = buscaÇinear(arr, tamanho, elemento);
+    if (resultado == -1) {
+        printf("Elemento não encontrado\n");
+    } else {
+        printf("Elemento encontrado no índice %d\n", resultado);
+    }
+    return 0;
+}
